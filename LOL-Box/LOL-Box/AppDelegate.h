e9772371b56f42e3fa74692411c00178d94b41b1
qw6703431 <mac@he.local>
2016-02-23 10:06:26 +0800

    2月23日 项目初始化

---
+++ LOL-Box/LOL-Box/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOL-Box
+//
+//  Created by ma c on 16/2/23.
+//  Copyright © 2016年 HE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
